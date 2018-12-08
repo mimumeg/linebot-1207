175d7f1b6dba9e976173443bd4c6b3305411c478
mimumeg <meg.stella555@gmail.com>
2018-12-08 15:13:37 +0900

    登録されたメッセージ以外の投稿はわんわんと返す。

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=talk[event.message.text]))
     else:
-        return "わんわん"
+        print("わんわん")
         # line_bot_api.reply_message(
         #     event.reply_token,
         #     TextSendMessage(text=event.message.text))
