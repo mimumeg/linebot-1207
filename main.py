1aba8883850eb123a29412e51071cc9b8d3a52c4
mimumeg <meg.stella555@gmail.com>
2018-12-08 14:46:33 +0900

    ファイル名変更

--- main.py
+++ main.py
@@ -8,41 +8,55 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage)
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
+talk = {
+    "こんにちは": "こんにちは！",
+    "好き": "私も❤️",
+}
 
-line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
+# 環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-@app.route("/push_sample")
-def push_sample():
-    """プッシュメッセージを送る"""
-    user_id = os.environ["USER_ID"]
-    line_bot_api.push_message(user_id, TextSendMessage(text="Hello World!"))
-
-    return "OK"
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError as e:
+    except InvalidSignatureError:
         abort(400)
 
-    return "OK"
+    return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=event.message.text))
+    if event.message.text in talk:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=talk[event.message.text]))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
+    #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
