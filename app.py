b89b5bd68cac0be6e90a34221e806af0f2c4913b
mimumeg <meg.stella555@gmail.com>
2018-12-09 11:11:40 +0900

    もふもふ画像も出て！

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 def response_message(event):
     # notesのCarouselColumnの各値は、変更してもらって結構です。
     notes = [CarouselColumn(
-        thumbnail_image_url="http://livedoor.blogimg.jp/mamesoku/imgs/e/5/e548fce7.jpg",
+        thumbnail_image_url="http://www.nhk.or.jp/mofumofu/weekly/photo/i_1805_1_4.jpg",
         title="もふ？非もふ？",
         text="直感的にお答え下さい！",
         actions=[
