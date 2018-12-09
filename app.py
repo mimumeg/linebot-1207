b499ce32fe70b4633297e0c6f6d3aec7d6cd1447
mimumeg <meg.stella555@gmail.com>
2018-12-09 10:08:50 +0900

    Buttonを貼り直し

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TemplateSendMessage, ButtonsTemplate)
+    MessageEvent, TextMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction, MessageAction, URIAction)
 
 app = Flask(__name__)
 
@@ -36,34 +36,53 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def response_message(event):
-    # notesのButtonsTemplateの各値は、変更してもらって結構です。
-    notes = [ButtonsTemplate(thumbnail_image_url="https://matome.naver.jp/odai/2142302304823215801/2142302490524539303",
-                             title="もふ？非もふ？",
-                             text="素直にお答え下さい♪",
-                             actions=[
-                                 {"type": "message", "label": "はい", "text": "https//www.google.com"},
-                                 {"type": "message", "label": "いいえ", "text": "https//www.google.com"}
-                             ])
-
-             # ButtonsTemplate(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
-             #                title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
-             #                text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
-             #                actions=[
-             #                    {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
-             #
-             # ButtonsTemplate(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
-             #                title="【ReleaseNote】タグ機能を追加しました。",
-             #                text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
-             #                actions=[
-             #                    {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])
-             ]
-
-    messages = TemplateSendMessage(
-        alt_text='template',
-        template=ButtonsTemplate(columns=notes),
+    buttons_template_message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://example.com/image.jpg',
+            title='Menu',
+            text='Please select',
+            actions=[
+                PostbackAction(
+                    label='postback',
+                    text='postback text',
+                    data='action=buy&itemid=1'
+                ),
+                MessageAction(
+                    label='message',
+                    text='message text'
+                ),
+                URIAction(
+                    label='uri',
+                    uri='http://example.com/'
+                )
+            ]
+        )
     )
-
-    line_bot_api.reply_message(event.reply_token, messages=messages)
+    # notesのCarouselColumnの各値は、変更してもらって結構です。
+    # notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
+    #                         title="【ReleaseNote】トークルームを実装しました。",
+    #                         text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
+    #                         actions=[{"type": "message","label": "サイトURL","text": "https://renttle.jp/notes/kota/7"}]),
+    #
+    #          CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
+    #                         title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
+    #                         text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
+    #                         actions=[
+    #                             {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
+    #
+    #          CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
+    #                         title="【ReleaseNote】タグ機能を追加しました。",
+    #                         text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
+    #                         actions=[
+    #                             {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])]
+    #
+    # messages = TemplateSendMessage(
+    #     alt_text='template',
+    #     template=CarouselTemplate(columns=notes),
+    # )
+    #
+    # line_bot_api.reply_message(event.reply_token, messages=messages)
 
 
 if __name__ == "__main__":
