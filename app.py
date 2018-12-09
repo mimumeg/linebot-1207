1083407ab478beea99860a5429793e7ac3998a05
mimumeg <meg.stella555@gmail.com>
2018-12-09 09:08:16 +0900

    カルーセルカラムから確認テンプレートに差し替えた。

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, CarouselColumn,
-    TemplateSendMessage, CarouselTemplate)
+    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, ConfirmTemplate, PostbackAction, MessageAction)
 
 app = Flask(__name__)
 
@@ -36,25 +35,47 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def response_message(event):
-    # notesのCarouselColumnの各値は、変更してもらって結構です。
-    notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
-                            title="【ReleaseNote】トークルームを実装しました。",
-                            text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
-                            actions=[
-                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/7"}]),
 
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
-                            title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
-                            text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
-                            actions=[
-                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
 
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
-                            title="【ReleaseNote】タグ機能を追加しました。",
-                            text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
-                            actions=[
-                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])]
+confirm_template_message = TemplateSendMessage(
+    alt_text='Confirm template',
+    template=ConfirmTemplate(
+        text='Are you sure?',
+        actions=[
+            PostbackAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageAction(
+                label='message',
+                text='message text'
+            )
+        ]
+    )
+)
+
+# @handler.add(MessageEvent, message=TextMessage)
+# def response_message(event):
+#     # notesのCarouselColumnの各値は、変更してもらって結構です。
+#     notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
+#                             title="【ReleaseNote】トークルームを実装しました。",
+#                             text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
+#                             actions=[
+#                                 {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/7"}]),
+
+# CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
+#                title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
+#                text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
+#                actions=[
+#                    {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
+#
+# CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
+#                title="【ReleaseNote】タグ機能を追加しました。",
+#                text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
+#                actions=[
+#                    {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])
+]
 
     messages = TemplateSendMessage(
         alt_text='template',
